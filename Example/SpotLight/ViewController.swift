00caf8d906cca200b036d8bf6d90484f2766786b
Cookiezby <zhubingyi1992@gmail.com>
2018-07-26 22:49:22 +0900

    Initial commit

---
+++ Example/SpotLight/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SpotLight
+//
+//  Created by Cookiezby on 07/26/2018.
+//  Copyright (c) 2018 Cookiezby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
